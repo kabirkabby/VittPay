6e628c9e13d78fd8d841e083065e15e0ac125e7e
Kabir Singh <singhkabir77@gmail.com>
2022-06-01 23:30:50 +0530

    Adding all my files

---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,12 @@
+
+require ('@nomiclabs/hardhat-waffle');
+
+module.exports = {
+  solidity: '0.8.0',
+  netwroks: {
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/UYFBUgWK-R5CmCG0bwkh0OVaCnFTTa2g",
+      accounts: ["eb223546c946ef1ff62db769e166a11ba885537b745aacf2b91178a33a71503a"]
+    }
+  }
+}
